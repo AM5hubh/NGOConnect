80a1703eb8025f6a59d230821a276c5996f0bb58
Shubham TS <ssahamate2020@gmail.com>
2024-08-25 15:46:53 +0530

    new database NGOConnect

--- Frontend/src/components/About/About.jsx
+++ Frontend/src/components/About/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='flex flex-row justify-evenly p-12'>
+    <div className='flex flex-row  justify-evenly p-12'>
       <div className='border w-1/2 bg-red-500'>
         <h1 className='text-center text-3xl text-bold m-3'>About</h1>
       </div>
