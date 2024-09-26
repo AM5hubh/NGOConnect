18c9289b78dc01af91d95e4f86d1da26da54a120
Shubham TS <ssahamate2020@gmail.com>
2024-09-26 13:20:12 +0530

    navbar changes

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -26,7 +26,7 @@ export default function Header() {
           </Link>
 
           <div
-            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1 gap-5"
+            className="justify-between items-center w-full lg:flex lg:w-auto lg:order-1 gap-5"
             id="mobile-menu-2"
           >
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 ">
@@ -64,7 +64,7 @@ export default function Header() {
                 <NavLink
                   to="/programs"
                   className={({ isActive }) =>
-                    `block py-2 pr-4 pl-3 duration-300 border-b border-gray-100 ${
+                    `py-2 pr-4 pl-3 duration-300 border-b border-gray-100 ${
                       isActive ? "text-orange-700" : "text-gray-700"
                     } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                   }
